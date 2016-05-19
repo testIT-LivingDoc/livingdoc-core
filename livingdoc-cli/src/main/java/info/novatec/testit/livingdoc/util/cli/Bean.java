928bd30ca14c6e948cc589119724ef4d53e4b647
Peter Kleimann <peter.kleimann@novatec-gmbh.de>
2016-05-19 12:53:05 +0200

    livingdoc-cli: fixed multi catch block for java 1.6

--- livingdoc-cli/src/main/java/info/novatec/testit/livingdoc/util/cli/Bean.java
+++ livingdoc-cli/src/main/java/info/novatec/testit/livingdoc/util/cli/Bean.java
@@ -1,15 +1,15 @@
 /* Copyright (c) 2007 Pyxis Technologies inc.
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
@@ -46,7 +46,9 @@ public class Bean {
     private void invoke(Method method, Object value) {
         try {
             method.invoke(target, value);
-        } catch (IllegalAccessException | InvocationTargetException e) {
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(String.format("%s to method %s", value, method.getName()), e);
+        } catch (InvocationTargetException e) {
             throw new IllegalArgumentException(String.format("%s to method %s", value, method.getName()), e);
         }
     }
